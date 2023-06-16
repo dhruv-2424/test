eca454626111ac52d84601f425f4bfc54b108d2a
dhruv-2424 <126473495+dhruv-2424@users.noreply.github.com>
2023-06-16 14:09:42 +0530

    test

---
+++ app.js
@@ -0,0 +1,21 @@
+var start = document.querySelector("#start");
+var web = document.querySelector("#web");
+
+start.addEventListener("click", ()=>{ 
+ 
+ var url = document.querySelector("#url").value;
+ if(url.includes("https")){
+ 
+ setInterval(function(){
+  web.src = url;
+  
+  }4000)
+ }
+
+ else{
+
+  alert("Enter a correct ur1");
+
+  }
+
+})
